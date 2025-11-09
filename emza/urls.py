72e50acc2ac8b89b25b5b70b15e886b318c93254
finusam <faheemsameer10@gmail.com>
2025-11-09 07:49:36 +0530

    fixed pwa custom template not displaying issue

--- emza/urls.py
+++ emza/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('pwa.urls')),
     path('stock/', include('stock.urls')),
     path('product/', include('product.urls')),
     path('sale/', include('sale.urls')),
-    path('', include('pwa.urls')),
+    path('', include('main.urls')),
 ]
+
