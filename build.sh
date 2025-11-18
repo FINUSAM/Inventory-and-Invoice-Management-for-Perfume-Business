495ef45bf26aa53fb623bf1a2b0952ad959ae3e4
finusam <faheemsameer10@gmail.com>
2025-11-19 00:33:20 +0530

    updated for vercel compatibility

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # Exit on error
 set -o errexit
 
+# Install dependencies
+pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
